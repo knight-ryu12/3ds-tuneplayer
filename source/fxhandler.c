23dd8ebccc239808e8a5cdb4fcbea053f1bbc34e
luigoalma <luigoalma@hotmail.com>
2019-08-29 14:37:24 +0100

    strcpy over snprintf when no format needed
    plus a few minor changes.

--- source/fxhandler.c
+++ source/fxhandler.c
@@ -2,6 +2,7 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <xmp.h>
 
@@ -14,42 +15,42 @@ bool handleFX(uint8_t fxt, uint8_t fxp, char *_arg1, bool isFT) {
             if (!fxp) {  // need to be real, not fake one
                 break;
             }
-            snprintf(_arg1, 6, "ARPeG");
+            strcpy(_arg1, "ARPeG");
             break;
         case 1:
             isBufferMem = true;
-            snprintf(_arg1, 6, "PORtU");
+            strcpy(_arg1, "PORtU");
             break;
         case 2:
             isBufferMem = true;
-            snprintf(_arg1, 6, "PORtD");
+            strcpy(_arg1, "PORtD");
             break;
         case 3:
             isBufferMem = true;
-            snprintf(_arg1, 6, "TPOrT");
+            strcpy(_arg1, "TPOrT");
             break;
         case 4:
             isBufferMem = true;
-            snprintf(_arg1, 6, "VIBrT");
+            strcpy(_arg1, "VIBrT");
             break;
         case 5:
             isBufferMem = true;
-            snprintf(_arg1, 6, "TONvS");
+            strcpy(_arg1, "TONvS");
             break;
         case 6:
             isBufferMem = true;
-            snprintf(_arg1, 6, "VIBvS");
+            strcpy(_arg1, "VIBvS");
             break;
         case 7:
             isBufferMem = true;
-            snprintf(_arg1, 6, "TREmO");
+            strcpy(_arg1, "TREmO");
             break;
         case 8:
-            snprintf(_arg1, 6, "PANsT");
+            strcpy(_arg1, "PANsT");
             break;
         case 9:
             isBufferMem = true;
-            snprintf(_arg1, 6, "OFStS");
+            strcpy(_arg1, "OFStS");
             break;
         case 0xa:
         case 0xa4:
@@ -58,32 +59,32 @@ bool handleFX(uint8_t fxt, uint8_t fxp, char *_arg1, bool isFT) {
             h = (fxp >> 4) & 0xF;
             l = fxp & 0xF;
             if (l == 0xF && h != 0)
-                snprintf(_arg1, 6, "VOLsU");
+                strcpy(_arg1, "VOLsU");
             else if (h == 0xf && l != 0)
-                snprintf(_arg1, 6, "VOLsD");
+                strcpy(_arg1, "VOLsD");
             else if (h == 0 && l != 0)  // S3M
-                snprintf(_arg1, 6, "VOLsD");
+                strcpy(_arg1, "VOLsD");
             else if (h != 0 && l == 0)
-                snprintf(_arg1, 6, "VOLsU");
+                strcpy(_arg1, "VOLsU");
             else if (h == 0xf && l == 0xf)  // ???
-                snprintf(_arg1, 6, "VOLsS");
+                strcpy(_arg1, "VOLsS");
             break;
         ft2:
             isBufferMem = true;
             if ((fxp & 0x0F) == 0 && (fxp >> 4 & 0xF) > 0)  // Up
-                snprintf(_arg1, 6, "VOLsU");
+                strcpy(_arg1, "VOLsU");
             else if (((fxp >> 4) & 0xF) == 0 && (fxp & 0x0F) > 0)  // Down
-                snprintf(_arg1, 6, "VOLsD");
+                strcpy(_arg1, "VOLsD");
             break;
 
         case 0xb:
-            snprintf(_arg1, 6, "POSjP");
+            strcpy(_arg1, "POSjP");
             break;
         case 0xc:
-            snprintf(_arg1, 6, "VOLsT");
+            strcpy(_arg1, "VOLsT");
             break;
         case 0xd:
-            snprintf(_arg1, 6, "PTBrK");
+            strcpy(_arg1, "PTBrK");
             break;
         case 0xe:;  // in FTII, E denotes "Extended" effect; that means we need break
                     // it down
@@ -95,75 +96,75 @@ bool handleFX(uint8_t fxt, uint8_t fxp, char *_arg1, bool isFT) {
                     break;
                 case 0x1:
                     isBufferMem = true;
-                    snprintf(_arg1, 6, "EPRtU");
+                    strcpy(_arg1, "EPRtU");
                     break;
                 case 0x2:
                     isBufferMem = true;
-                    snprintf(_arg1, 6, "EPRtD");
+                    strcpy(_arg1, "EPRtD");
                     break;
                 case 0x3:
                     // Not supported widely...?
                     break;
                 case 0x4:
-                    snprintf(_arg1, 6, "STVwF");
+                    strcpy(_arg1, "STVwF");
                     break;
                 case 0x5:
-                    snprintf(_arg1, 6, "STFtV");
+                    strcpy(_arg1, "STFtV");
                     break;
                 case 0x6:  //Pattern loop is the special thing...
                     if (l == 0)
-                        snprintf(_arg1, 6, "PTLpS");
+                        strcpy(_arg1, "PTLpS");
                     else
-                        snprintf(_arg1, 6, "PTLpG");
+                        strcpy(_arg1, "PTLpG");
                     break;
                 case 0xa:
-                    snprintf(_arg1, 6, "EFVsU");
+                    strcpy(_arg1, "EFVsU");
                     break;
                 case 0xb:
-                    snprintf(_arg1, 6, "EFVsD");
+                    strcpy(_arg1, "EFVsD");
                     break;
                 case 0xc:
-                    snprintf(_arg1, 6, "ENCuT");
+                    strcpy(_arg1, "ENCuT");
                     break;
                 case 0xd:
-                    snprintf(_arg1, 6, "EDElY");
+                    strcpy(_arg1, "EDElY");
                     break;
             }
             break;
 
         case 0xa3:
         case 0xf:
-            snprintf(_arg1, 6, "SPDsT");
+            strcpy(_arg1, "SPDsT");
             break;
 
         case 0x10:
-            snprintf(_arg1, 6, "GVOlS");
+            strcpy(_arg1, "GVOlS");
             break;
 
         case 0x11:
             isBufferMem = true;
-            snprintf(_arg1, 6, "GVOlS");
+            strcpy(_arg1, "GVOlS");
             break;
 
         case 0x15:
-            snprintf(_arg1, 6, "EVPsS");
+            strcpy(_arg1, "EVPsS");
             break;
 
         case 0x1b:
             isBufferMem = true;
-            snprintf(_arg1, 6, "RETrG");
+            strcpy(_arg1, "RETrG");
             break;
 
         // S3M/IT
         case 0x8d:
-            snprintf(_arg1, 6, "SURrD");
+            strcpy(_arg1, "SURrD");
             break;
         case 0x87:
-            snprintf(_arg1, 6, "SETtP");
+            strcpy(_arg1, "SETtP");
             break;
             // Btw; Txx effect is kinda special
 	case 0x80:
-	    snprintf(_arg1,6,"TRLvL");
+	    strcpy(_arg1, "TRLvL");
 	    break;
 
 
