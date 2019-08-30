b630178fc4a33b083506f56053c02cba572fa762
luigoalma <luigoalma@hotmail.com>
2019-08-30 14:21:46 +0100

    Decrease copy operations
    and buffer string show_generic_info

--- include/fxhandler.h
+++ include/fxhandler.h
@@ -1,4 +1,4 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-bool handleFX(uint8_t fxt, uint8_t fxp, char *_arg1, bool isFT);
+bool handleFX(uint8_t fxt, uint8_t fxp, const char **p_arg1, char *_arg1, bool isFT);
